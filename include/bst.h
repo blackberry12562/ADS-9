4035538d5512cc9aa1bf8d931f2888da09acc751
blackberry12562 <116704068+blackberry12562@users.noreply.github.com>
2023-05-30 22:38:52 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,6 +1,7 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
+#include <iostream>
 template <typename T>
 class BST {
  private:
@@ -10,26 +11,24 @@ class BST {
         Node* l;
         Node* r;
     };
-        Node* r;
-    };
     Node* root = nullptr;
-    int searchNodeTree(Node* node, const T& value) {
+    int searchNodeTree(Node* root, const T& value) {
         if (node == nullptr) {
             return 0;
-        } else if (node->val > value) {
-            return searchNodeTree(node->l, value);
-        } else if (node->val < value) {
-            return searchNodeTree(node->r, value);
+        } else if (root->val > value) {
+            return searchNodeTree(root->l, value);
+        } else if (root->val < value) {
+            return searchNodeTree(root->r, value);
         } else {
-            return node->count;
+            return root->count;
         }
         return 0;
     }
-    int getDepthTree(Node* node) {
-        if (node == nullptr)
+    int getDepthTree(Node* root) {
+        if (root == nullptr)
             return 0;
-        int rDepth = getDepthTree(node->r),
-            lDepth = getDepthTree(node->l);
+        int rDepth = getDepthTree(root->r),
+            lDepth = getDepthTree(root->l);
         return (lDepth > rDepth) ? lDepth + 1 : rDepth + 1;
     }
     Node* newNodeTree(Node* root, const T& value) {
@@ -38,12 +37,11 @@ Expand All
         return searchNodeTree(node, value);
     }
     int depth() {
-        return getDepthTree(node) - 1;
+        return getDepthTree(root) - 1;
     }
     void newNode(const T& value) {
-        node = newNodeTree(node, value);
+        node = newNodeTree(root, value);
     }
 };
 
-#endif  // INCLUDE_BST_H_
 #endif  // INCLUDE_BST_H_
